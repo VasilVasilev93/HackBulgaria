a5d0e3e731497bef1aa4f369bd6401a05bc56052
VasilVasilev93 <vasilvasilev093@gmail.com>
2014-10-18 20:55:20 +0300

    Sudoku finished

--- week02nd/sudoku.py
+++ week02nd/sudoku.py
@@ -1,10 +1,11 @@
-def elements_unique(listA):
-    if len(set(listA)) == len(listA):
-        return True
-    return False
+def is_increasing(seq):
+    for k in range(0, len(seq) - 1):
+        if seq[k] >= seq[k + 1]:
+            return False
+    return True
 
 
-def reversed_matrix(matrix):
+def transponse_matrix(matrix):
     r = []
     rm = []
 
@@ -17,21 +18,61 @@ def reversed_matrix(matrix):
     return rm
 
 
-def sudoku_solved(sudoku):
+def check_rows_columns(matrix):
+    checkList = []
     count = 0
-    rev = reversed_matrix(sudoku)
-    for k in range(0, 9):
-        if elements_unique(sudoku[k]) and elements_unique(rev[k]):
-            count += 1
 
-    m = []
-    n = []
-    for i in range(3, 6):
-        for j in range(3, 6):
-            m.append(sudoku[i][j])
-        n.append(list(m))
-        m = []
-    return n
+    for row in range(0, 9):
+        for col in range(0, 9):
+            checkList.append(matrix[row][col])
+        checkList = sorted(checkList)
+        if is_increasing(checkList):
+            count += 1
+            checkList = []
+
+    transponsedmatrix = transponse_matrix(matrix)
+
+    for row in range(0, 9):
+        for col in range(0, 9):
+            checkList.append(transponsedmatrix[row][col])
+        checkList = sorted(checkList)
+        if is_increasing(checkList):
+            count += 1
+            checkList = []
+
+    if count == 18:
+        return True
+
+
+def create_submatrix_3x3(matrix):
+    checkList = []
+    count = 0
+    i = 0
+    j = 3
+    x = 0
+    y = 3
+    for submatrix in range(0, 3):
+        for row in range(i, j):
+            for col in range(x, y):
+                checkList.append(matrix[row][col])
+            x += 3
+            y += 3
+            checkList = sorted(checkList)
+            if is_increasing(checkList):
+                count += 1
+                checkList = []
+        i += 3
+        j += 3
+        x = 0
+        y = 0
+    if count == 9:
+        return True
+
+
+def sudoku_solved(matrix):
+    if check_rows_columns(matrix) and create_submatrix_3x3(matrix):
+        return True
+    return False
 
 print (sudoku_solved([
     [4, 5, 2, 3, 8, 9, 7, 1, 6],
