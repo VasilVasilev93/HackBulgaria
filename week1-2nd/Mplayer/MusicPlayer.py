0f06028909da353740b1d783ae5941f42c985477
VasilVasilev93 <vasilvasilev093@gmail.com>
2014-11-07 00:06:18 +0200

    Task not fully completed yet.

---
+++ week1-2nd/Mplayer/MusicPlayer.py
@@ -0,0 +1,11 @@
+import pygame
+
+
+file = '03 Hans Zimmer & Lorne Balfe - Main Menu.mp3'
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load(file)
+pygame.mixer.music.play()
+
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
